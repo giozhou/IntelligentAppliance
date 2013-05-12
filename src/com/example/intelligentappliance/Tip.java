9a92fe76c3c88ab9c3f4f614c9be7245d7652ada
giozhou <gio.zhou@gmail.com>
2013-05-12 11:35:45 +0800

    the first excutable version on new operation

--- src/com/example/intelligentappliance/Tip.java
+++ src/com/example/intelligentappliance/Tip.java
@@ -1,5 +1,7 @@
 package com.example.intelligentappliance;
 
+import java.io.IOException;
+
 import com.example.intelligentappliance.ConnectedThread;
 
 import android.app.Activity;
@@ -11,36 +13,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class Tip extends Dialog {
 	private LinearLayout layoutWork;
 	private LinearLayout layoutWait;
+	private TextView textViewShowMessage;
 	private String deviceName;
+	private ProgressBar progressBar1;
 	private int position;
 	public static BluetoothSocket btSocket;
 	private Switch switch1;
-	
+
 	public Handler myHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			int status = Integer.parseInt(msg.getData().getString("v"));
 			switch (status) {
 			case 0:
-				layoutWork.setVisibility(View.VISIBLE);
-				layoutWait.setVisibility(View.INVISIBLE);
+				textViewShowMessage.setText("连接失败!");
+				progressBar1.setVisibility(View.INVISIBLE);
 				break;
 			case 1:
 				layoutWork.setVisibility(View.VISIBLE);
 				layoutWait.setVisibility(View.INVISIBLE);
 				break;
 			case 2:
+				layoutWait.setVisibility(View.INVISIBLE);
 				break;
 			case 3:
+				layoutWait.setVisibility(View.INVISIBLE);
 				break;
 			}
 			super.handleMessage(msg);
@@ -65,21 +74,22 @@ public class Tip extends Dialog {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.tip);
 
-		layoutWait = (LinearLayout) this
-				.findViewById(R.id.layoutWait);
-		layoutWork = (LinearLayout) this
-				.findViewById(R.id.layoutWork);
-		Switch switch1 = (Switch)this.findViewById(R.id.switch1);
-		
+		layoutWait = (LinearLayout) this.findViewById(R.id.layoutWait);
+		layoutWork = (LinearLayout) this.findViewById(R.id.layoutWork);
+		switch1 = (Switch) this.findViewById(R.id.switch1);
+		textViewShowMessage = (TextView) this.findViewById(R.id.textView2);
+		progressBar1 = (ProgressBar) this.findViewById(R.id.progressBar1);
+
 		layoutWork.setVisibility(View.INVISIBLE);
 
 		new Thread(new ConnectThread(MainActivity.pairedDevices[position],
 				Tip.this, position)).start();
-		
+
 		switch1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
 				layoutWait.setVisibility(View.VISIBLE);
+				textViewShowMessage.setText("正在切换……");
 
 				String code = isChecked ? "a1#" : "a0#";
 				new Thread(new ConnectedThread(btSocket, code.getBytes(),
@@ -88,6 +98,14 @@ public class Tip extends Dialog {
 		});
 	}
 	
-	
 
+	@Override
+	protected void onStop(){
+		try {
+			btSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
